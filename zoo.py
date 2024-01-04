43bc40f588043e308add434f007678da520be653
BNBendi <bnbendi2008@gmail.com>
2024-01-04 13:24:13 +0100

    hozzáadás kész

--- zoo.py
+++ zoo.py
@@ -1,4 +1,21 @@
-zoo = list()
+def addAnimal( myName):
+    animal=dict()
+    found = False
+    for a in zoo:
+        if myName  in  a.keys():
+            a[myName] += 1
+            found=True
+            #break
+    if not found:
+        animal[myName] = 1 
+        zoo.append(animal)
+    
+
+
+
+zoo = [{"kecske": 1}, {"ló": 1}]
+animal=dict()
+
 
 print("EZ egy állatkert.")
 print("Állat hozzáadása (1)- elvétele (2) - Kilépés (0)")
@@ -8,19 +25,10 @@ while select !="0":
     select  = input("Mit szeretne tenni?")
     if select !="0":
       if select == "1":
-          #animal = dict()
             name = input("AZ állat neve: ")
-            if name  not in  animal.keys():
-              animal[name] = 1
-              zoo.append(animal)
-            else:
-               for a in zoo:
-                   a[name] += 1
-            animal = dict()
-          
-          
-for a in zoo:
-    print(a)      
+            addAnimal(name)
+                             
+                
+print(f"Első: {zoo} ")
         
               
-print("vége")
